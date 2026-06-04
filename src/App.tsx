b047f2e76596162a792cc15848beceb18c3d7fb7
Paradox-09-coder <qayumovabdu2009@gmail.com>
2026-06-04 15:48:52 +0500

    Add lang prop to SecurityView component

--- src/App.tsx
+++ src/App.tsx
@@ -688,6 +688,7 @@ export default function App() {
                   credentials={credentials}
                   onEdit={handleEditTrigger}
                   showToast={showToast}
+                  lang={lang}
                 />
               )}
 
